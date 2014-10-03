e4a23378e35363a3d2a74bc52dec7c61fd0be08d
Bernard <bernardxf@gmail.com>
2014-10-03 11:04:33 -0300

    Inserção do menu lateral
    Inserção do menu lateral

---
+++ development/scripts/main.js
@@ -0,0 +1,11 @@
+function $(val){
+  return document.querySelector(val);
+}
+
+function s(val){
+  return document.querySelectorAll(val);
+}
+$('.menu').addEventListener('click', function() {
+	$("#container").className = ( $("#container").className == 'active' )? '' : 'active';
+	$(".menu").className = ( $(".menu").className == 'menu menu-active' )? 'menu' : 'menu menu-active';
+}, false);
